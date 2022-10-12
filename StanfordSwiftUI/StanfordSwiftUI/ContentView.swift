68b21455d08dbbfdd77b1a0dce61e6c69c78a84e
astsi <40372146+astsi@users.noreply.github.com>
2022-10-12 10:13:46 +0200

    Add initial version of the project

---
+++ StanfordSwiftUI/StanfordSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StanfordSwiftUI
+//
+//  Created by Anastasija Mihajlenko on 10/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Ana!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StanfordSwiftUI/StanfordSwiftUI/StanfordSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordSwiftUIApp.swift
+//  StanfordSwiftUI
+//
+//  Created by Anastasija Mihajlenko on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StanfordSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
